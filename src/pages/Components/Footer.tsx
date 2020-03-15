bc8629e18eb611539f0347a6556b946e84052d03
kyogen-hekii <kyogen.hekii@gmail.com>
2020-03-15 20:20:47 +0900

    Footerをfunctional componentに書き換え

--- src/pages/Components/Footer.tsx
+++ src/pages/Components/Footer.tsx
@@ -1,82 +1,46 @@
-//prepare
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
-// import SimpleFetch from "../../api/SimpleFetch"
-// import { getUser } from "../../api/queries"
+
+// #region constants
+const pages = [
+  {
+    id: 'NOTE',
+    to: '/note',
+  },
+  {
+    id: 'HOME',
+    to: '/home',
+  },
+  {
+    id: 'ANKI',
+    to: '/anki',
+  },
+]
+// #endregion
 
 type Props = {
   history: any
-  currentPage: string
-  //openModal: Function
 }
-class PageFooter extends Component<Props> {
-  // #region state
-  // TODO: local stateをglobal stateに昇格
-  state: any = {
-    user: {},
-  }
-  // #endregion
-
-  // #region componentDidMount
-  async componentDidMount() {
-    // const user = await SimpleFetch(getUser(1))
-    // this.setState({ user })
-  }
-  // #endregion
-
-  // #region handler
-  handleClickPopUser = () => {
-    // TODO
-    const { user } = this.state
-    window.alert(`my name is ${user.name}`)
-  }
-  // #endregion
-
-  // #region render
-  render() {
-    const pages = [
-      {
-        id: 'NOTE',
-        to: '/note',
-      },
-      {
-        id: 'HOME',
-        to: '/home',
-      },
-      {
-        id: 'ANKI',
-        to: '/anki',
-      },
-    ]
-    const { history } = this.props
-    return (
-      <div
-        style={{ position: 'fixed', left: 0, bottom: 0, backgroundColor: 'white', width: '100%' }}
-      >
-        <div className="pv20" style={{ display: 'flex', justifyContent: 'space-around' }}>
-          {pages.map(p =>
-            history.location.pathname === p.to ? (
-              <div key={p.id}>{p.id}</div>
-            ) : (
-              <Link key={p.id} to={p.to}>
-                <div>{p.id}</div>
-              </Link>
-            ),
-          )}
-          {/* <button onClick={this.handleClickPopUser}>Callendar</button> */}
-        </div>
-      </div>
-    )
-  }
-  // #endregion
-}
-
+const PageFooter = ({ history }: Props) => (
+  <div style={{ position: 'fixed', left: 0, bottom: 0, backgroundColor: 'white', width: '100%' }}>
+    <div className="pv20" style={{ display: 'flex', justifyContent: 'space-around' }}>
+      {pages.map(p =>
+        history.location.pathname === p.to ? (
+          <div key={p.id}>{p.id}</div>
+        ) : (
+          <Link key={p.id} to={p.to}>
+            <div>{p.id}</div>
+          </Link>
+        ),
+      )}
+      {/* <button onClick={this.handleClickPopUser}>Callendar</button> */}
+    </div>
+  </div>
+)
 const mapStateToProps = (state: any) => ({
   currentPage: state.currentPage,
 })
 
-const mapDispatchToProps = {
-  //openModal,
-}
+const mapDispatchToProps = {}
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PageFooter))
