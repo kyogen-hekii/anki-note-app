57e8bf9ec69a0886c3961dec56af105028fbe2bc
kyogen-hekii <kyogen.hekii@gmail.com>
2020-03-16 00:50:18 +0900

    Footerのsvgアイコン追加

--- src/pages/Components/Footer.tsx
+++ src/pages/Components/Footer.tsx
@@ -1,20 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
+import FlashcardImg from '../../assets/svg/flashcard.svg'
+import FlashcardImgOn from '../../assets/svg/flashcard-on.svg'
+import HomeImg from '../../assets/svg/home.svg'
+import HomeImgOn from '../../assets/svg/home-on.svg'
+import NoteImg from '../../assets/svg/note.svg'
+import NoteImgOn from '../../assets/svg/note-on.svg'
 
 // #region constants
 const pages = [
   {
     id: 'NOTE',
     to: '/note',
+    logo: NoteImg,
+    logoOn: NoteImgOn,
   },
   {
     id: 'HOME',
     to: '/home',
+    logo: HomeImg,
+    logoOn: HomeImgOn,
   },
   {
     id: 'ANKI',
     to: '/anki',
+    logo: FlashcardImg,
+    logoOn: FlashcardImgOn,
   },
 ]
 // #endregion
@@ -27,10 +39,16 @@ const PageFooter = ({ history }: Props) => (
     <div className="pv20" style={{ display: 'flex', justifyContent: 'space-around' }}>
       {pages.map(p =>
         history.location.pathname === p.to ? (
-          <div key={p.id}>{p.id}</div>
+          <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+            <img key={p.id} src={p.logoOn} alt={p.id} width={50} height={50} />
+            <span>{p.id}</span>
+          </div>
         ) : (
           <Link key={p.id} to={p.to}>
-            <div>{p.id}</div>
+            <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+              <img src={p.logo} alt={p.id} width={50} height={50} />
+              <span>{p.id}</span>
+            </div>
           </Link>
         ),
       )}
